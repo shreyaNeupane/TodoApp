f9b13fb60b7a771c08e963ff2912154dcdfdc4be
shreyaNeupane <shreyaneupane49@gmail.com>
2025-12-12 08:10:53 +0545

    Landing page

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import todoimage from '../../assets/images/todoimage.jpg'
+import './Landing.css';
 
 const Landing = () => {
   return (
-    <div>
-      this is landing page
+    <div className='hero'>
+      <div className="intro-text">
+        <h1>
+          <span className="tagline1">Organize work and life</span><br/>
+          <span className="tagline2">finally</span>
+        </h1>
+        <p>
+          todo helps you finish you boost productivity and improve focus...
+        </p>
+        <Link className="btn red" to="/register"> Register Now</Link>
+        <Link className="btn blue" to="/login">Login</Link>
+      </div>
+      <div>
+        <img src={todoimage} alt="todoimage"  width={"100%"} height={515}/>
+      </div>
+     
     </div>
   )
 }
