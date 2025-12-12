6e62263f07134eba653f8482b496f81ec60580bf
shreyaNeupane <shreyaneupane49@gmail.com>
2025-12-12 12:24:58 +0545

    login and register frontend

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React ,{useState} from "react";
+import { Link } from "react-router-dom";
+import "./AuthStyle.css";
 
 const Login = () => {
-  return (
-    <div>
-      this is login page
-    </div>
-  )
-}
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
 
-export default Login
+  //login function
+  const loginHandler = (e) => {
+    e.preventDefault()
+    try{
+ alert("Login Data " + email + " " + password);
+    }catch(error){
+      console.log(error)
+    }
+   
+  }
+  return (
+    <>
+      <div className="form-container">
+        <div className="form">
+          <div className="mb-3">
+            <p className="welcome">Welcome back</p>
+            <p className="login">Login to get started</p>
+          </div>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-bottom">
+            <p className="text-center">
+              Not A User? Please <Link to="/register">Register</Link>
+            </p>
+            <button type="submit" className="login-btn" onClick={loginHandler}>
+              LOGIN
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./AuthStyle.css";
 
 const Register = () => {
-  return (
-    <div>
-      this is register page
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Register
+  //login function
+  const registerHandler = (e) => {
+    e.preventDefault();
+    try {
+      alert("Login Data " + email + " " + password + username);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="form-container">
+      <div className="form">
+        <div className="mb-3">
+          <p className="welcome">Create Account</p>
+          <p>Sign up to get started</p>
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-bottom">
+          <p className="text-center">
+            Already Have an account? Please <Link to="/login">Login</Link>
+          </p>
+          <button type="submit" className="login-btn" onClick={registerHandler}>
+            LOGIN
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
